d12296d7d5225d9846dcd7b1c3176fa1df43096b
anupsingh7068 <115370935+anupsingh7068@users.noreply.github.com>
2023-07-10 23:57:31 +0530

    Update store_data.php

--- store_data.php
+++ store_data.php
@@ -1,5 +1,5 @@
 <?php
-
+// Connect to the database
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,42 +7,44 @@ $dbname = "Employee_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST['name'];
+    $department = $_POST['department'];
+    $age = $_POST['age'];
+    $salary = $_POST['salary'];
 
+    if (!empty($name) && !empty($department) && !empty($age) && !empty($salary) ) {
+        $sql = "INSERT INTO employees (Name, DepartmentID, Age, Salary) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("siii", $name, $department, $age, $salary);
+        if ($stmt->execute()) {
+            $message = "Data stored successfully.";
+            $class = "success";
+        } else {
 
-$name = $_POST['name'];
-$department = $_POST['department'];
-$age = $_POST['age'];
-$salary = $_POST['salary'];
-
-
-$sql = "INSERT INTO employees (Name, DepartmentID, Age, Salary) VALUES (?, ?, ?, ?)";
-
-
-$stmt = $conn->prepare($sql);
-
-$stmt->bind_param("siii", $name, $department, $age, $salary);
-
-if ($stmt->execute()) {
-    $message = "Data stored successfully.";
-    $class = "success";
+            $message = "Error storing data: " . $stmt->error;
+            $class = "danger";
+        }
+        $stmt->close();
+    } else {
+        $message = "Please enter correct value.";
+        $class = "danger";
+    }
 } else {
-    $message = "Error storing data: " . $stmt->error;
-    $class = "error";
+    $message = "Only post menthod allowed.";
+    $class = "danger";
 }
-
-$stmt->close();
 $conn->close();
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Data Entry</title>
-    <!-- Add Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .container {
@@ -61,9 +63,10 @@ $conn->close();
         </div>
     </div>
     <script>
-        setTimeout(function () {
+        setTimeout(function() {
             window.location.href = "index.php";
-        }, 2000); 
+        }, 2000); // Redirect after 2 seconds
     </script>
 </body>
+
 </html>

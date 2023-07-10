68e1d3e9a5febd2db95e2f4f87980f0d1747e3e9
anupsingh7068 <115370935+anupsingh7068@users.noreply.github.com>
2023-07-10 10:02:47 +0530

    Update employee_db.sql

--- employee_db.sql
+++ employee_db.sql
@@ -1,41 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 09, 2023 at 02:40 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.28
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `employee_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `departments`
---
 
 CREATE TABLE `departments` (
   `DepartmentID` int(11) NOT NULL,
   `Department` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `departments`
---
+
 
 INSERT INTO `departments` (`DepartmentID`, `Department`) VALUES
 (1, 'IT'),
@@ -43,11 +16,7 @@ INSERT INTO `departments` (`DepartmentID`, `Department`) VALUES
 (3, 'Marketing'),
 (4, 'Finance');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `employees`
---
 
 CREATE TABLE `employees` (
   `EmployeeID` int(11) NOT NULL,
@@ -57,33 +26,17 @@ CREATE TABLE `employees` (
   `Salary` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `departments`
---
 ALTER TABLE `departments`
   ADD PRIMARY KEY (`DepartmentID`);
 
---
--- Indexes for table `employees`
---
+
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`EmployeeID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `employees`
---
 ALTER TABLE `employees`
   MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
